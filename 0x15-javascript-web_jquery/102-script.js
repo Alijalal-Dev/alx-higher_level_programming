e2b5d6d80a87b1a85bc5f5cd90d1fbcbf43f733e
Alijalal-Dev <aj0546006@gmail.com>
2025-02-19 06:28:27 -0800

    task 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,8 @@
-.ready(function () {
+$('document').ready(function () {
   const url = 'https://www.fourtonfish.com/hellosalut/?';
-  .click(function () {
-    $.get(url + $.param({ lang: .val() }), function (data) {
-      .html(data.hello);
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
     });
   });
 });
